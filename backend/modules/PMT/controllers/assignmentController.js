b2ef14c1fa1da9f270218e2167238daa589fd88b
Thejesh-wotsatbot <thejeshwar.sy@wotsabot.com>
2024-09-20 13:14:31 +0530

    Backend Structure defined.-Tej

--- backend/modules/PMT/controllers/assignmentController.js
+++ backend/modules/PMT/controllers/assignmentController.js
@@ -1,6 +1,6 @@
-import User from '../../models/PMT_models/user.js';
-import Story from '../../models/PMT_models/story.js';
-import Task from '../../models/PMT_models/task.js';
+import User from '../models/user.js';
+import Story from '../models/story.js';
+import Task from '../models/task.js';
 
 export const getUserAssignments = async (req, res) => {
     try {
