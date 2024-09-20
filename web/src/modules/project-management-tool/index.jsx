980f182f03209935555fea7af75b2596492ece9b
Thejesh-wotsatbot <thejeshwar.sy@wotsabot.com>
2024-09-20 11:05:30 +0530

    Changes in the navigation-Tej

--- web/src/modules/project-management-tool/index.jsx
+++ web/src/modules/project-management-tool/index.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import EpicComponent from "./components/Epics";
 import StoryComponent from "./components/Story";
 import TaskComponent from "./components/TaskComponent ";
