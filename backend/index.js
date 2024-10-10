1fe8d8f085cbd583399b82f7dde0fdd747841c9c
Aravind-wotSABot <aravind@wotsabot.com>
2024-10-11 01:29:43 +0530

    Add workflow's all backend codes expect integration and integration2

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,40 @@ import {
   cityRoute,
   addressRoute,
   leadRoute,
+  // Workflow Imports
+  aiInsightRoute,
+  associatesRoute,
+  auditLogRoute,
+  clarificationsRoute,
+  coAccountsRoute,
+  commentsRoute,
+  currenciesRoute,
+  currencyRatesRoute,
+  dashboardConfigRoute,
+  dimensionRoute,
+  documentRoute,
+  enquiriesRoute,
+  escalationRoute,
+  generalLedgerRoute,
+  // glOpeningRoute,
+  incidentRoute,
+  incidentHeaderRoute,
+  // integrationRoutes
+  // integrationRoutes2
+  workflowLeadRoute,
+  ledgerSummaryRoute,
+  notificationRoute,
+  periodsRoute,
+  workflowRoleRoute,
+  ruleRoute,
+  slaRoute,
+  standardRulesRoute,
+  timeTrackingRoute,
+  transactionTypesRoute,
+  workflowUserRoute,
+  versionControlRoute,
+  workflowRoute,
+  workflowTemplateRoute,
 } from "./imports.js";
 
 const app = express();
@@ -33,7 +67,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-// Routes
+// PMT Routes
 app.use("/api/epics", epicRoutes);
 // app.use("/api/stories", storyRoutes);
 app.use("/api/tasks", taskRoutes);
@@ -42,7 +76,7 @@ app.use("/api/userGroups", userGroupRoutes);
 // app.use("/api", assignmentRoutes);
 app.use("/api/owners", ownerRoutes);
 
-//CRM API
+// CRM Routes
 app.use("/api", authRoute);
 app.use("/api", divisionRoute);
 app.use("/api", departmentRoute);
@@ -57,6 +91,41 @@ app.use("/api", stateRoute);
 app.use("/api", cityRoute);
 app.use("/api", addressRoute);
 app.use("/api", leadRoute);
+
+// Workflow Routes
+app.use("/api", aiInsightRoute);
+app.use("/api", associatesRoute);
+app.use("/api", auditLogRoute);
+app.use("/api", clarificationsRoute);
+app.use("/api", coAccountsRoute);
+app.use("/api", commentsRoute);
+app.use("/api", currenciesRoute);
+app.use("/api", currencyRatesRoute);
+app.use("/api", dashboardConfigRoute);
+app.use("/api", dimensionRoute);
+app.use("/api", documentRoute);
+app.use("/api", enquiriesRoute);
+app.use("/api", escalationRoute);
+app.use("/api", generalLedgerRoute);
+// app.use("/api", glOpeningRoute);
+app.use("/api", incidentRoute);
+app.use("/api", incidentHeaderRoute);
+// integrationRoutes
+// integrationRoutes2
+app.use("/api", workflowLeadRoute);
+app.use("/api", ledgerSummaryRoute);
+app.use("/api", notificationRoute);
+app.use("/api", periodsRoute);
+app.use("/api", workflowRoleRoute);
+app.use("/api", ruleRoute);
+app.use("/api", slaRoute);
+app.use("/api", standardRulesRoute);
+app.use("/api", timeTrackingRoute);
+app.use("/api", transactionTypesRoute);
+app.use("/api", workflowUserRoute);
+app.use("/api", versionControlRoute);
+app.use("/api", workflowRoute);
+app.use("/api", workflowTemplateRoute);
 
 app.get("/", (req, res) => {
   res.send("BackEnd is running.");
